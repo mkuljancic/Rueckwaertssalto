1455286dfe522bf7260b605accc68527f1bc4380
Sefa <Sefa@Benim>
2015-01-17 20:32:45 +0100

    First Code

---
+++ src/oeztuerk_kuljancic/Prototyp.java
@@ -0,0 +1,32 @@
+package oeztuerk_kuljancic;
+
+public class Prototyp {
+
+	public static void main(String[] args) {
+//		//var metadata = require('jdbc-metadata');
+//
+//		var jdbcConfig = {
+//		    libpath: __dirname + '/../jar/mysql-connector-java-5.1.32-bin.jar',
+//		    drivername: 'com.mysql.jdbc.Driver',
+//		    url: 'jdbc:mysql://localhost:3306/test',
+//		    user: 'root',
+//		    password: ''
+//		};
+//
+//		var jdbcMetadata = new metadata(jdbcConfig);
+//
+//		jdbcMetadata.metadata(function (err, metadata) {
+//
+//		    console.log('Getting tables...');
+//
+//		    jdbcMetadata.tables(null, function (err, tables) {
+//		        console.log(tables);
+//
+//		        jdbcConn.close(function(err) {
+//		          console.log('Connection closed');
+//		        });
+//		    });
+//		});
+	}
+
+}
