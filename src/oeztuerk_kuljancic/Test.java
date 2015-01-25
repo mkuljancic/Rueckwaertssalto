4a5a4c6570875ccd223d05b11f4ef453f37b5c8c
Sefa <soeztuerk01@student.tgm.ac.at>
2015-01-25 20:24:49 +0100

    Testklasse noch fertig dokumentieren

--- src/oeztuerk_kuljancic/Test.java
+++ src/oeztuerk_kuljancic/Test.java
@@ -3,7 +3,12 @@ package oeztuerk_kuljancic;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
-
+/**
+ * Diese Klasse testet die Klasse Rueckwaertssalto
+ * 
+ * @author Öztürk Sefa, Kuljancic Mirza
+ *
+ */
 public class Test {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException,UnsupportedEncodingException {
 		Rueckwaertssalto n = new Rueckwaertssalto();
